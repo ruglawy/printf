cee46f7a629af52a6ec270631cbce30270df74a8
git <git@github.com>
2023-04-23 14:06:07 -0700

    Added r specifier + fixed issues

--- cases.c
+++ cases.c
@@ -78,10 +78,24 @@ int caseD(int num)
  */
 int caseR(const char *string)
 {
-	int i, length = (int)strlen(string);
-	int j = length - 1;
-	char *reverse = malloc(sizeof(char) * (length + 1));
+	int i, length, j;
+	char *reverse;
 
+	if (string == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+
+	length = (int)strlen(string);
+	j = length - 1;
+	reverse = malloc(sizeof(char) * (length + 1));
+
+	if (reverse == NULL)
+	{
+		free(reverse);
+		exit(98);
+	}
 	for (i = 0; i < length; i++)
 	{
 		reverse[i] = string[j];
