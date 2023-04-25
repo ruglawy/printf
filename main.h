84bcfccb7d2871f62ce9da830170455070aa1e5a
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-25 13:42:54 -0700

    Edited write() to have a buffer of 1024 max

--- main.h
+++ main.h
@@ -4,16 +4,17 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-/**
- * _printf - printf() replica
- * @format: string to be printed.
- *
- * Return: length of string printed
- */
+#include <limits.h>
+#include <stdio.h>
+
 int _printf(const char *format, ...);
 int caseS(const char *string);
 int caseC(int ch);
 int casePercentage();
 int caseD(int num);
+int caseU(int num);
+int int_to_binary(int num);
+void write_data();
 int caseR(const char *string);
 #endif
+
